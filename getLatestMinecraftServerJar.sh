1c10e1d5e514d52dc59ba01f82c7236ecf0c71e8
M-D-M <m.d.mcnamara@gmail.com>
2017-04-18 16:10:27 -0400

    Modified server retrieval script to use latest JSON from Minecraft site.

--- getLatestMinecraftServerJar.sh
+++ getLatestMinecraftServerJar.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
 
-downloadurl="https://minecraft.net/en/download/server"
-serverurl=""
-loc=$([[ -n $1 ]] && printf $1 || printf "/tmp/minecraft_server.jar")
+JSON_URL='https://launchermeta.mojang.com/mc/game/version_manifest.json'
+LOCAL_FILE=
+SERVER_URL=
+SERVER_FILE_SHA1=
 
-if [[ -a $loc ]]; then
-	printf "$loc exists -- moving to ${loc}.old\n"
-	mv $loc ${loc}.old
-fi
+function getJSONData {
+        echo $1 | egrep -o "\"$2\": ?[^\}]*(\}|\")" | sed "s/\"$2\"://"
+}
 
-printf "Getting download URL for minecraft server...\n"
+function getJSONVal {
+        echo $1 | egrep -o "\"$2\": ?\"[^\"]*" | sed "s/\"$2\"://" | tr -d '{}" '
+}
 
-serverurl=`curl -s $downloadurl | egrep -io 'https.*versions\/(.*)\/minecraft_server.\1.jar'`
+function grabLatestMCServerJar {
+        MCJSON=$(curl -s $JSON_URL)
+        LATEST_VER=$(getJSONVal "$(getJSONData "$MCJSON" latest)" release)
 
-printf "URL = "$serverurl"\n"
+        LATEST_URL_DATA=$(echo $MCJSON | egrep -o "\"id\":\"${LATEST_VER}\"[^}]*")
+        LATEST_VER_URL=$(getJSONVal "$LATEST_URL_DATA" url)
 
-printf "Downloading server jar...\n"
+        MCURLJSON=$(curl -s $LATEST_VER_URL)
+	SERVER_JAR_NAME=minecraft_server_${LATEST_VER}.jar
 
-wget -q -O $loc $serverurl
+        SERVER_URL=$(getJSONVal "$(getJSONData "$MCURLJSON" server)" url)
+        SERVER_FILE_SHA1=$(getJSONVal "$(getJSONData "$MCURLJSON" server)" sha1)
+
+	LOCAL_FILE=$([[ -n $1 ]] && printf $1 || printf /tmp/$SERVER_JAR_NAME)
+
+	if [[ -a "${LOCAL_FILE}" ]]; then
+		printf "${LOCAL_FILE} exists -- moving to ${LOCAL_FILE}.old\n"
+		mv ${LOCAL_FILE} ${LOCAL_FILE}.old
+	fi
+
+	printf "\nDownloading ${SERVER_JAR_NAME}..."
+	wget -q -O ${LOCAL_FILE} ${SERVER_URL}
+	printf "\nFile downloaded at: ${LOCAL_FILE}"
+
+	printf "\nChecking sha1sum of ${LOCAL_FILE}..."
+	DOWNLOAD_FILE_SHA1SUM=$(sha1sum ${LOCAL_FILE} | awk '{print $1}')
+
+	if [ $(sha1sum "${LOCAL_FILE}" | awk '{print $1}') != "${SERVER_FILE_SHA1}" ]; then
+		printf "\nSHA1 of downloaded file does not match!"
+		rm ${LOCAL_FILE}
+	else
+		printf "\nSHA1 matches!"
+	fi
+}
+
+grabLatestMCServerJar "$@"
